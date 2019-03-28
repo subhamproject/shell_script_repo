a1ae79abdbbb057cc512d15ac03f6097a94ebf48
Subham Mandal <subhammandal01@gmail.com>
2019-03-28 19:54:53 +0530

    Update user-create-del.sh

--- user-create-del.sh
+++ user-create-del.sh
@@ -14,23 +14,25 @@ else
   echo "$USERNAME:$PASSWORD" |chpasswd  > /dev/null
   [ $? -eq 0 ] && echo "Password has been set for user $USERNAME"
 fi
+unset USERNAME
 }
 
 user_del () {
- while [ -z "${USERNAME}" ];do
+ while [ -z "${USER_NAME}" ];do
  echo -e "Please enter the user name you wish to delete?:- \c"
- read USERNAME
- [ -z "${USERNAME}" ] && echo "Username Cannot be empty,Please try again."
+ read USER_NAME
+ [ -z "${USER_NAME}" ] && echo "Username Cannot be empty,Please try again."
 done
- grep -q $USERNAME /etc/passwd >> /dev/null
+ grep -q $USER_NAME /etc/passwd >> /dev/null
  if [ $? -eq 0 ] ;then
-   echo -e "User $USERNAME exist,Are you sure to proceed with deletion(yes/no)?:- \c"
+   echo -e "User $USER_NAME exist,Are you sure to proceed with deletion(yes/no)?:- \c"
    read RES
-   [ "${RES}" == "Y" -o "${RES}" == "y" -o "${RES}" == "yes" -o "${RES}" == "YES" ] && userdel -r $USERNAME > /dev/null 2>&1 && echo "User $USERNAME has been deleted successfully"
-   [ "${RES}" == "N" -o "${RES}" == "n" -o "${RES}" == "no" -o "${RES}" == "NO" ] && echo "You have chosen not to delete..Skipping"
+   [ "${RES}" == "Y" -o "${RES}" == "y" -o "${RES}" == "yes" -o "${RES}" == "YES" ] && userdel -r $USER_NAME > /dev/null 2>&1 && echo "User $USER_NAME has been deleted successfully"
+   [ "${RES}" == "N" -o "${RES}" == "n" -o "${RES}" == "no" -o "${RES}" == "NO" ] && echo "You have chosen not to delete $USER_NAME..Skipping"
  else
    echo "User does not exist"
  fi
+unset USER_NAME
 }
 PS3='Please select your choice: '
 options=("Add User" "Delete User" "Quit")
