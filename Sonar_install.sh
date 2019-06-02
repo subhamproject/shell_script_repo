42168432533c69b34c1de51473d1d0d4dba2ad74
Subham Mandal <subhammandal01@gmail.com>
2019-06-03 00:07:40 +0530

    Update Sonar_install.sh

--- Sonar_install.sh
+++ Sonar_install.sh
@@ -2,7 +2,7 @@
 
 DIR="/sonarqube"
 
-function install_sonar() 
+function install_sonar () 
 {
 yum remove java-1.7.0-openjdk -y
 yum install java-1.8.0 -y
@@ -12,7 +12,7 @@ useradd -c "Sonarqube Default Account" -s /bin/bash sonar && chown -R sonar:sona
 sed -i 's|#sonar.jdbc.username=.*|sonar.jdbc.username=sonar|; s|#sonar.jdbc.password=.*|sonar.jdbc.password=sonar|; s|#sonar.jdbc.url=jdbc:postgresql.*|sonar.jdbc.url=jdbc:postgresql://localhost/sonar|' $DIR/sonar/conf/sonar.properties
 }
 
-function install_postgres()
+function install_postgres ()
 {
 yum install postgresql96-server postgresql96-contrib -y && mkdir -p /postgres && chown -R postgres:postgres /postgres && su - postgres bash -c 'initdb /postgres' && \
 su - postgres bash -c 'pg_ctl -D /postgres -l logfile start' && echo "postgres" | passwd --stdin postgres && sleep 5
@@ -25,13 +25,13 @@ CREATE DATABASE sonar OWNER sonar;
 EOF
 }
 
-function start_sonar()
+funtion start_sonar ()
 {
 sleep 5
 chown -R sonar:sonar /sonarqube/ && \
-ln -s $DIR/sonar/bin/linux-x86-64/sonar.sh /etc/init.d/sonar && sed -i 's|#RUN_AS_USER=.*|RUN_AS_USER=sonar|' /etc/init.d/sonar && \
-sed -i 's|#RUN_AS_USER=.*|RUN_AS_USER=sonar|' $DIR/sonar/bin/linux-x86-64/sonar.sh && chkconfig sonar on
-$DIR/sonar/bin/linux-x86-64/sonar.sh start
+ln -s $DIR/sonar/bin/linux-x86-64/sonar.sh /etc/init.d/sonar && sed -i 's|#RUN_AS_USER=.*|RUN_AS_USER=sonar|; s|^WRAPPER_CMD=.*|WRAPPER_CMD="/sonarqube/sonar/bin/linux-x86-64/wrapper"|; s|^WRAPPER_CONF=.*|WRAPPER_CONF="/sonarqube/sonar/conf/wrapper.conf"|' /etc/init.d/sonar && \
+chkconfig sonar on
+service sonar start
 }
 
 install_sonar
