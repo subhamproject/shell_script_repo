6bec98670033bf7be2325a18620defc00a2f4380
Subham Mandal <subhammandal01@gmail.com>
2019-06-03 23:16:08 +0530

    Create Install_sonar.sh

---
+++ Install_sonar.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+DIR="/sonarqube"
+
+function install_sonar() 
+{
+yum remove java-1.7.0-openjdk -y
+yum install java-1.8.0 -y
+[ ! -d $DIR ] && mkdir -p $DIR
+cd $DIR && wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.7.zip && unzip sonarqube-*.zip && rm -rf sonarqube-*.zip && mv sonarqube-7.* sonar
+useradd -c "Sonarqube Default Account" -s /bin/bash sonar && chown -R sonar:sonar $DIR/sonar && echo "sonar - nofile  65536" >> /etc/security/limits.conf && echo "sonar - nproc 2048" >> /etc/security/limits.conf
+sed -i 's|#sonar.jdbc.username=.*|sonar.jdbc.username=sonar|; s|#sonar.jdbc.password=.*|sonar.jdbc.password=sonar|; s|#sonar.jdbc.url=jdbc:postgresql.*|sonar.jdbc.url=jdbc:postgresql://localhost/sonar|' $DIR/sonar/conf/sonar.properties
+}
+
+function install_postgres()
+{
+yum install postgresql96-server postgresql96-contrib -y && mkdir -p /postgres && chown -R postgres:postgres /postgres && su - postgres bash -c 'initdb /postgres' && \
+su - postgres bash -c 'pg_ctl -D /postgres -l logfile start' && echo "postgres" | passwd --stdin postgres && sleep 5
+su - postgres << EOF
+createuser sonar
+psql
+ALTER USER sonar WITH ENCRYPTED password 'sonar';
+CREATE DATABASE sonar OWNER sonar;
+\q
+EOF
+cat <<! >> temp
+#!/bin/bash
+TYPE=\$1
+case $TYPE in
+  status)
+  su - postgres bash -c 'pg_ctl -D /postgres -l logfile status'
+  ;;
+  start)
+  su - postgres bash -c 'pg_ctl -D /postgres -l logfile start'
+  ;;
+  stop)
+  su - postgres bash -c 'pg_ctl -D /postgres -l logfile stop'
+  ;;
+ esac
+!
+chmod a+x /usr/bin/postgresql && ln -s /usr/bin/postgresql /etc/init.d/postgres
+}
+
+function start_sonar()
+{
+sleep 5
+chown -R sonar:sonar /sonarqube/ && \
+ln -s $DIR/sonar/bin/linux-x86-64/sonar.sh /etc/init.d/sonar && sed -i 's|#RUN_AS_USER=.*|RUN_AS_USER=sonar|; s|^WRAPPER_CMD=.*|WRAPPER_CMD="/sonarqube/sonar/bin/linux-x86-64/wrapper"|; s|^WRAPPER_CONF=.*|WRAPPER_CONF="/sonarqube/sonar/conf/wrapper.conf"|; s|^PIDDIR=.*|PIDDIR="/sonarqube/sonar/"|' /etc/init.d/sonar && \
+chkconfig sonar on
+service sonar start
+}
+
+install_sonar
+install_postgres
+start_sonar
