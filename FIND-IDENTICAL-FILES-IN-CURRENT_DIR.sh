aa7864d51c6c24248603c0c038305881006d38ff
Subham Mandal <subhammandal01@gmail.com>
2019-10-25 00:30:55 +0530

    Create FIND-IDENTICAL-FILES-IN-CURRENT_DIR.sh

---
+++ FIND-IDENTICAL-FILES-IN-CURRENT_DIR.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## Pass the dirname as argument to this script
+[ $# -ne 1 ] && echo "Usage: $0 <dirname>" && exit 1
+
+DIR_NAME=${1}
+
+FILES="$(find ${DIR_NAME} ! -empty -type f -exec md5sum {} + | sort | uniq -w32 -dD|while read FILE_SHA FILE_NAME
+do
+echo
+echo $FILE_SHA
+echo $FILE_NAME
+done |awk '!_[$0]++')"  && \
+[ -n "${FILES}" ] && \
+echo -e "Please find the identical files which are of same Content\nFiles listed underneath each sha value are identical having same content" && echo&& \
+echo $FILES |tr ' ' '\n' || echo "No identical files found"
