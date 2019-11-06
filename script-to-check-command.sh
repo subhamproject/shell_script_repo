c24a63f4b6b1fe49d8daacca7f49dc2288c1e0ac
Subham Mandal <subhammandal01@gmail.com>
2019-11-06 11:21:10 +0530

    Create script-to-check-command.sh

---
+++ script-to-check-command.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function check {
+  "$@"
+  status=$?
+  if [ $status -ne 0 ]; then
+    echo "ERROR: Encountered error (${status}) while running the following:" >&2
+    echo "           $@"  >&2
+    echo "       (at line ${BASH_LINENO[0]} of file $0.)"  >&2
+    echo "       Aborting." >&2
+    exit $status
+  fi
+}
+
+check mkdir -p /tmp/subham
