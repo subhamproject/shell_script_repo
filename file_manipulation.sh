deed752037bb7ead5e4d897bac58c35b05252461
Subham Mandal <subhammandal01@gmail.com>
2020-01-20 23:19:53 +0530

    Update file_manipulation.sh

--- file_manipulation.sh
+++ file_manipulation.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+OLD_IFS=$IFS
+
 IFS=$'\n'
 shopt -s xpg_echo
 
 FILE="${1:-file.txt}"
+
 c1=''
 c2=''
 c3=''
 c4=''
 c5=''
+
+[ ! -f ${FILE} ] && { echo "$FILE not found" ; exit 1 ;} || :
 
 for line in $(cat $FILE);do
 LINE1=$(echo $line|awk -F' ' '{print $1}')
@@ -30,3 +35,5 @@ fi
 done
 echo "c1\tc2\tc3\tc4\tc5"
 echo "$c1\t$c2\t$c3\t$c4\t$c5"
+
+IFS=$OLD_IFS
