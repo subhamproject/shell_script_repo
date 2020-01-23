181d95421cdcc8caf5794e564a9b6aee15c05717
Subham Mandal <subhammandal01@gmail.com>
2020-01-23 15:01:20 +0530

    Update download_with_spinnar.sh

--- download_with_spinnar.sh
+++ download_with_spinnar.sh
@@ -3,12 +3,8 @@
 shopt -s xpg_echo
 
 echo -n "Downloading nexus from source:  "
-
-while true; do for X in '-' '/' '|' '\'; do echo -en "\b$X"; sleep 0.1; done; done &
-
+while true; do for X in '/' '-' '\' '|'; do echo -en "\b$X"; sleep 0.1; done; done &
 PID=$!
 trap 'kill $PID' EXIT
-
 [ ! -e latest-unix.tar.gz ] && wget -q http://download.sonatype.com/nexus/3/latest-unix.tar.gz || { echo "\nNexus file already exit in current path." ; exit 1 ; }
-
 [ $? -eq 0 ] && echo "\nNexus download was successful." && kill $PID
