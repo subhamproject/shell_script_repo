e4fe2645f1f077dbe608fc6d91514724b2a0254c
Subham Mandal <subhammandal01@gmail.com>
2020-02-05 14:52:48 +0530

    Create variable-substitution.sh

---
+++ variable-substitution.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for LINE in $(cat file.txt);do
+SERVICENAME=$LINE
+MONITOR_NAME=${SERVICENAME:+docker-$SERVICENAME}
+echo "Service name is:- $SERVICENAME"
+echo "Monitor name is:- $MONITOR_NAME"
+done
