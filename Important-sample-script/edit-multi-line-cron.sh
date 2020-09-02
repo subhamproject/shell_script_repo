eab9f8fd4ea2925938ccbeac3779129d4613f338
Subham Mandal <subhammandal01@gmail.com>
2020-09-02 14:02:02 +0530

    Update edit-multi-line-cron.sh

--- Important-sample-script/edit-multi-line-cron.sh
+++ Important-sample-script/edit-multi-line-cron.sh
@@ -3,16 +3,18 @@
 IFS=$'\n'
 shopt -s xpg_echo
 
-SCRIPT_NAME="link.sh|subham.sh|sink.sh|pink.sh"
+SCRIPT_NAME="link.sh"
 
 function edit_cron() {
 CRON="$1"
 if [[ -z "$CRON" ]];then
-echo "Cron entry not present on host: $(hostname)"
+:
 else
   if [[ ${CRON:0:1} == '#' ]];then
      echo "Cron entry \"$CRON\" is commented on hosts: $(hostname), uncommenting it"
      echo "$(crontab -l|grep -v ${CRON##*/} )\n${CRON/##/}" | crontab -
+     else
+     echo "Crons \"$CRON\" is not commented on $(hostname)"
      fi
 fi
 }
