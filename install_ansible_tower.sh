ddc983c144d4456f864bbe1a7835e412f37498d8
Subham Mandal <subhammandal01@gmail.com>
2022-09-10 01:15:54 +0530

    Update install_ansible_tower.sh

--- install_ansible_tower.sh
+++ install_ansible_tower.sh
@@ -2,6 +2,13 @@
 
 #https://releases.ansible.com/ansible-tower/setup/?extIdCarryOver=true&sc_cid=701f2000001OH7YAAW
 
+function must_be_root() {
+if [[ $EUID -ne 0 ]]; then
+   echo "You must be root to run this script." 1>&2
+   exit 1
+fi
+}
+
 function log() {
     echo "$1" >&2
 }
@@ -16,8 +23,8 @@ function check_exist() {
     [ ! -z "$(command -v ansible)" ] || die "THE 'ansible' COMMAND IS MISSING - PLEASE INSTALL AND TRY AGAIN"
 }
 
-
 function check_os_supported() {
+must_be_root
 case $(egrep '^(NAME)=' /etc/os-release|cut -d'=' -f2|sed 's|"||g') in
 "Amazon Linux")
 printf "THIS OS IS NOT SUPPORTED FOR ANSIBLE TOWER.."
@@ -32,7 +39,6 @@ exit 1
 esac
 }
 
-
 function check_hardware_requirement() {
 check_os_supported
 [[ ! $(grep '^processor' /proc/cpuinfo | sort -u | wc -l) -ge 2 ]] && { echo "PLEASE UPGRADE YOUR SERVER WITH AT LEAST 2 CORE CPU - AND TRY AGAIN" ; exit 1 ; }
@@ -42,15 +48,11 @@ RAM_GB=$(expr $RAM_KB / 1024 / 1024)
 [[ ! $(df -h|grep -vE 'tmpfs|devtmpfs|docker'|sed '1d'|awk '{print $2}'|sed 's|G||g'|head -1) -ge 20 ]] &&  { echo "YOU NEED TO HAVE AT LEAST 20GB DISK SIZE FOR ANSIBLE TOWER TO RUN - CREATE AND TRY AGAIN" ; exit 1 ; }
 }
 
-
-
 function main() {
 check_hardware_requirement
 check_exist
 DATA_DIR="/data"
-
 TOWER_VERSION=${1:-ansible-tower-setup-3.7.0-4.tar.gz}
-
 curl  https://releases.ansible.com/ansible-tower/setup/${TOWER_VERSION} -o /tmp/tower.tar.gz
 [ ! -d $DATA_DIR ] && mkdir -p $DATA_DIR
 cd $_ && tar xvzf /tmp/tower.tar.gz --strip 1
