ec43c249e5fbab9c0fafc836fc2e7fe82e1defcf
Subham Mandal <subhammandal01@gmail.com>
2022-09-13 23:38:08 +0530

    Create error-handling-shell.sh

---
+++ error-handling-shell.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# @brief actions required to be conducted before updates are applied
+#        and/or servers are rebooted.  
+logFile="/tmp/pre_update.log"
+###########################
+# Begin Functions         #
+function log_msg {
+  current_time=$(date "+%Y-%m-%d %H:%M:%S.%3N")
+  log_level=$1
+  # all arguments except for the first one, since that is the level
+  log_msg="${@:2}"
+  echo "[$current_time] $log_level - $log_msg" >> $logFile
+}
+function log_error {
+  log_msg "ERROR" "$@"
+}
+function log_info {
+  log_msg "INFO " "$@"
+}
+function log_debug {
+  log_msg "DEBUG" "$@"
+}
+# End Functions           #
+###########################
+###########################
+# Begin Body              #
+errorCheck=0
+cat /dev/null > $logFile
+log_info "========================================================"
+log_info "= Pre-update status for $HOSTNAME"
+log_info "========================================================"
